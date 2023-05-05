ce45dcbb06454c99341e09c46a47ad6cf95cff42
maxberglund2 <106820018+maxberglund2@users.noreply.github.com>
2023-05-05 12:03:26 +0200

    Delete blog directory

--- blog/footer.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo '<footer style="padding: 20px; text-align: center;">
-<div style="max-width: 960px; margin: 0 auto;">
-  <p>&copy; 2023 Critter. All rights reserved.</p>
-</div>
-</footer>
-</body></html>';
-?>
